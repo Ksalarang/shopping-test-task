02506d9321b23398da5d1471d2073eb20c29be68
Diyar Taikenov <drtaikenov@gmail.com>
2025-03-07 18:07:44 +0500

    Add movement input for editor

---
+++ Assets/Scripts/Shopping/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using Shopping.UserInput;
+using UnityEngine;
+using Zenject;
+
+namespace Shopping.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Inject]
+        private IMovementInput _movementInput;
+
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        [SerializeField]
+        private Camera _camera;
+
+        [SerializeField]
+        private float _speed = 10f;
+
+        private void Update()
+        {
+            var cameraTransform = _camera.transform;
+            var movementInput = _movementInput.Value;
+            var cameraForward = cameraTransform.forward;
+            var cameraRight = cameraTransform.right;
+
+            cameraForward.y = cameraRight.y = 0;
+            var direction = (cameraForward * movementInput.y + cameraRight * movementInput.x).normalized;
+
+            _rigidbody.MovePosition(transform.position + direction * (_speed * Time.deltaTime));
+        }
+    }
+}
--- Assets/Scripts/Shopping/UserInput/Joystick.cs
+++ Assets/Scripts/Shopping/UserInput/Joystick.cs
@@ -5,7 +5,7 @@ namespace Shopping.UserInput
 {
     public class Joystick : MonoBehaviour, IMovementInput, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
-        public Vector2 Value => _inputVector;
+        public Vector2 Value => _value;
 
         [SerializeField]
         private RectTransform _handle;
@@ -13,22 +13,45 @@ namespace Shopping.UserInput
         [SerializeField]
         private RectTransform _background;
 
-        private Vector2 _inputVector;
+        private Vector2 _value;
+        private bool _dragging;
 
-        public void OnPointerDown(PointerEventData eventData) {}
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _dragging = true;
+        }
 
         public void OnDrag(PointerEventData eventData)
         {
             var delta = (Vector3)eventData.position - _background.position;
             delta = Vector3.ClampMagnitude(delta, _background.rect.width / 2f);
             _handle.localPosition = delta;
-            _inputVector = delta.normalized;
+            _value = delta.normalized;
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
+            _dragging = false;
             _handle.localPosition = Vector3.zero;
-            _inputVector = Vector2.zero;
+            _value = Vector2.zero;
         }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (_dragging)
+            {
+                return;
+            }
+
+            var pressingLeft = Input.GetKey(KeyCode.A);
+            var pressingRight = Input.GetKey(KeyCode.D);
+            var pressingForward = Input.GetKey(KeyCode.W);
+            var pressingBackward = Input.GetKey(KeyCode.S);
+
+            _value.x = pressingLeft == pressingRight ? 0 : pressingRight ? 1 : -1;
+            _value.y = pressingForward == pressingBackward ? 0 : pressingForward ? 1 : -1;
+        }
+#endif
     }
 }
