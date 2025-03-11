d37524a35c08a5f20cdaacea51f49581da25056a
Diyar Taikenov <drtaikenov@gmail.com>
2025-03-11 17:32:58 +0500

    Add cashier to shop

---
+++ Assets/Scripts/Shopping/Utils/Cashier.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Shopping.Utils
+{
+    public class Cashier : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        [SerializeField]
+        private Transform _player;
+
+        private void Update()
+        {
+            _rigidbody.MoveRotation(Quaternion.LookRotation((transform.position - _player.position).normalized));
+        }
+    }
+}
