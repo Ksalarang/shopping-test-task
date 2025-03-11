bb13ef32c5779fd242022fe24616cb7db0a9ec0c
Diyar Taikenov <drtaikenov@gmail.com>
2025-03-11 14:20:36 +0500

    Add mobile touch input

--- Assets/Scripts/Shopping/UserInput/MobileCameraInput.cs
+++ Assets/Scripts/Shopping/UserInput/MobileCameraInput.cs
@@ -11,7 +11,7 @@ namespace Shopping.UserInput
     [UsedImplicitly]
     public class MobileCameraInput : ICameraInput, ITickable
     {
-        public event Action<Collider> OnClick; //todo:
+        public event Action<Collider> OnClick;
         public Vector2 Delta => _delta;
 
         [Inject]
@@ -20,8 +20,12 @@ namespace Shopping.UserInput
         [Inject]
         private EventSystem _eventSystem;
 
+        [Inject]
+        private Camera _camera;
+
         private Vector2 _delta;
         private bool _overUi;
+        private bool _moved;
         private int _currentIndex = -1;
 
         public void Tick()
@@ -49,19 +53,44 @@ namespace Shopping.UserInput
                         if (i == _currentIndex)
                         {
                             _delta = touch.deltaPosition;
+
+                            if (_delta != Vector2.zero)
+                            {
+                                _moved = true;
+                            }
                         }
                         break;
                     case TouchPhase.Ended:
+                        if (i == _currentIndex)
+                        {
+                            if (_moved == false)
+                            {
+                                OnClick?.Invoke(PhysicsUtils.Raycast(_camera, touch.position));
+                            }
+
+                            Reset();
+                        }
+                        break;
                     case TouchPhase.Canceled:
                         if (i == _currentIndex)
                         {
-                            _delta = Vector2.zero;
-                            _overUi = false;
-                            _currentIndex = -1;
+                            Reset();
                         }
                         break;
                 }
             }
+
+            if (Input.touchCount == 0)
+            {
+                Reset();
+            }
+        }
+
+        private void Reset()
+        {
+            _delta = Vector2.zero;
+            _overUi = _moved = false;
+            _currentIndex = -1;
         }
     }
 }
